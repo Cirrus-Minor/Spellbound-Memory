3c03bc810692f8e004377daca29b17398227d662
Enfant Voodoo <EnfantVoodoo777@gmail.com>
2023-12-04 15:56:53 +0100

    Add start screen

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,15 +1,23 @@
 extends Node2D
 
+var start_screen = preload("res://Scenes/starting_screen.tscn")
 var main_fight = preload("res://Scenes/main_fight.tscn")
+
+var _start_screen
 var _fight_zone
 
 func _ready():
-	# At the moment, only fighting
-	generate_fight_zone()
+	_start_screen = start_screen.instantiate()
+	_start_screen.connect("start_game", _on_start_game)
+	add_child(_start_screen)
 
 func _process(delta):
 	pass
-
+	
+func _on_start_game():
+	_start_screen.queue_free()
+	generate_fight_zone()
+			
 func _on_level_up():
 	_fight_zone.queue_free()
 	generate_fight_zone()
