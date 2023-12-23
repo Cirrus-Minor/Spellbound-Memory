d0b3f6b91d129849168eed8c2c8786ed127dbf47
Enfant Voodoo <EnfantVoodoo777@gmail.com>
2023-12-23 13:39:34 +0100

    Add Wave effect on usage text

--- Scripts/starting_screen.gd
+++ Scripts/starting_screen.gd
@@ -3,7 +3,6 @@ extends Node2D
 signal start_game
 
 @onready var titleSprite = $"GameTitle"
-@onready var UsageText = $"Usage"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,7 +16,6 @@ func _physics_process(delta):
 	titleSprite.rotation_degrees = 5 * cos(Time.get_ticks_msec() * 0.001)
 	var scale_factor = 0.85 + 0.02 * sin(Time.get_ticks_msec() * 0.002)
 	titleSprite.scale = Vector2(scale_factor, scale_factor)
-	UsageText.modulate.a = 0.5 + (1 + cos(Time.get_ticks_msec() * 0.008)) / 2.0
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton && event.button_index == 1 && event.pressed:
