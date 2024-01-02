47fb35cc57a42397752966eceb70efb42b79ccdf
Enfant Voodoo <EnfantVoodoo777@gmail.com>
2024-01-02 19:10:05 +0100

    Add sounds for monster 4

--- Scripts/monster_04.gd
+++ Scripts/monster_04.gd
@@ -4,6 +4,13 @@ extends MonsterBase
 
 @onready var anim = $Sprite/AnimatedSprite2D
 
+@onready var sound_attack_1 = $Sounds/Attack1
+@onready var sound_attack_2 = $Sounds/Attack2
+@onready var sound_hurt_1 = $Sounds/Hurt1
+@onready var sound_hurt_2 = $Sounds/Hurt2
+@onready var sound_die = $Sounds/Die
+@onready var sound_power = $Sounds/Power
+
 var timer_power = 0.0
 
 # 1 = attack
@@ -36,17 +43,31 @@ func manipulate():
 func on_dying():
 	super()
 	anim.play("die")
+	await get_tree().create_timer(0.2).timeout
+	sound_die.pitch_scale = randf_range(0.9, 1.2)
+	sound_die.play()
 
 func on_hurting():
 	super()
 	anim.play("hurt")
+	var sound = sound_hurt_1
+	if randi_range(1, 2) == 2: sound = sound_hurt_2
+	await get_tree().create_timer(0.2).timeout
+	sound.pitch_scale = randf_range(0.9, 1.2)
+	sound.play()
 	
 func on_attack():
 	anim.play("attack")
+	var sound = sound_attack_1
+	if randi_range(1, 2) == 2: sound = sound_attack_2
+	sound.pitch_scale = randf_range(0.9, 1.2)
+	sound.play()
 	
 func on_power():
 	anim.play("power")
 	timer_power = 1.0
+	sound_power.pitch_scale = randf_range(0.9, 1.2)
+	sound_power.play()
 
 func on_idle():
 	anim.play("idle")
